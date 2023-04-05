231549a81331db527dcf1b7ab96186d7b0aa5316
StEn <github@steven-england.info>
2023-04-05 14:27:16 +0200

    refactor: add fixture

--- tests/unittests/apps/photos/services/test_photo_consumer_service.py
+++ tests/unittests/apps/photos/services/test_photo_consumer_service.py
@@ -1,10 +1,42 @@
+import pytest
 from django.conf import settings
 from pytest_mock import MockerFixture
 
+from app.common.services.file_system_service_interface import (
+    FileSystemServiceInterface,
+)
 from app.photos.services import photo_consumer_service
 
 
-def test_consume_copies_file_to_destination(mocker: MockerFixture) -> None:
+@pytest.fixture(scope="module", name="pcs_mock")
+def photo_consumer_service_mock(
+    mocker: MockerFixture,
+) -> photo_consumer_service.PhotoConsumerService:
+    file_system_service_mock = mocker.Mock(spec=FileSystemServiceInterface)
+    pcs = photo_consumer_service.PhotoConsumerService(
+        file_system_service=file_system_service_mock,
+    )
+    return pcs
+
+
+# def test_consume_copies_file_to_destination(
+#    pcs_mock: photo_consumer_service.PhotoConsumerService,
+# ) -> None:
+#    settings.PHOTOS_REPO_ROOTDIR = "/test"
+#    mocked_copy_file = mocker.patch(
+#        "app.common.services.file_system_service.FileSystemService.copy_file",
+#    )
+#
+#    pcs_mock.print_something()
+#    pcs_mock.consume(src_file_path="/test/test.jpg")
+#
+#    mocked_copy_file.assert_called_once_with(
+#        src_file_path="/test/test.jpg",
+#        dst_file_path="/test",
+#    )
+
+
+def test_consume_copies_file_to_destination2(mocker: MockerFixture) -> None:
     settings.PHOTOS_REPO_ROOTDIR = "/test"
     mocked_copy_file = mocker.patch(
         "app.common.services.file_system_service.FileSystemService.copy_file",
