0e541fe5a53f131235ac1423f3bb0035a97c7347
StEn <github@steven-england.info>
2023-04-05 14:33:56 +0200

    test(framework): switch from pytest-mock to mockito

--- tests/unittests/apps/photos/services/test_photo_consumer_service.py
+++ tests/unittests/apps/photos/services/test_photo_consumer_service.py
@@ -1,52 +1,42 @@
 import pytest
 from django.conf import settings
-from pytest_mock import MockerFixture
+from mockito import mock
 
 from app.common.services.file_system_service_interface import (
     FileSystemServiceInterface,
 )
-from app.photos.services import photo_consumer_service
+from app.photos.services.photo_consumer_service import PhotoConsumerService
 
 
-@pytest.fixture(scope="module", name="pcs_mock")
-def photo_consumer_service_mock(
-    mocker: MockerFixture,
-) -> photo_consumer_service.PhotoConsumerService:
-    file_system_service_mock = mocker.Mock(spec=FileSystemServiceInterface)
-    pcs = photo_consumer_service.PhotoConsumerService(
+@pytest.fixture(scope="module", name="pcs")
+def photo_consumer_service() -> PhotoConsumerService:
+    file_system_service_mock = mock(spec=FileSystemServiceInterface)
+    pcs = PhotoConsumerService(
         file_system_service=file_system_service_mock,
     )
-    return pcs
+    return pcs  # noqa: WPS331
 
 
-# def test_consume_copies_file_to_destination(
-#    pcs_mock: photo_consumer_service.PhotoConsumerService,
-# ) -> None:
-#    settings.PHOTOS_REPO_ROOTDIR = "/test"
-#    mocked_copy_file = mocker.patch(
-#        "app.common.services.file_system_service.FileSystemService.copy_file",
-#    )
-#
-#    pcs_mock.print_something()
-#    pcs_mock.consume(src_file_path="/test/test.jpg")
-#
-#    mocked_copy_file.assert_called_once_with(
-#        src_file_path="/test/test.jpg",
-#        dst_file_path="/test",
-#    )
-
-
-def test_consume_copies_file_to_destination2(mocker: MockerFixture) -> None:
+def test_consume_copies_file_to_destination(expect) -> None:
     settings.PHOTOS_REPO_ROOTDIR = "/test"
-    mocked_copy_file = mocker.patch(
-        "app.common.services.file_system_service.FileSystemService.copy_file",
-    )
-    pcs = photo_consumer_service.PhotoConsumerService()
-
-    pcs.print_something()
-    pcs.consume(src_file_path="/test/test.jpg")
-
-    mocked_copy_file.assert_called_once_with(
+    fss_mock = mock(FileSystemServiceInterface)
+    expect(fss_mock, times=1).copy_file(
         src_file_path="/test/test.jpg",
         dst_file_path="/test",
     )
+    pcs = PhotoConsumerService(
+        file_system_service=fss_mock,
+    )
+
+    pcs.consume(src_file_path="/test/test.jpg")
+
+
+def test_consume_copies_file_to_destination2(pcs: PhotoConsumerService, expect):
+    settings.PHOTOS_REPO_ROOTDIR = "/test"
+
+    expect(pcs.file_system_service, times=1).copy_file(
+        src_file_path="/test/test.jpg",
+        dst_file_path="/test",
+    )
+
+    pcs.consume(src_file_path="/test/test.jpg")
