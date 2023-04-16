566401f2d4eafef8f11f5ad6e9f6dbdee7b7b469
StEn <github@steven-england.info>
2023-04-16 14:51:26 +0200

    feat(pas): add photo validation

--- app/photos/services/photo_analyzer_service.py
+++ app/photos/services/photo_analyzer_service.py
@@ -1,4 +1,6 @@
+import filetype
 from imagededup.methods import CNN, AHash, DHash, PHash, WHash
+from PIL import Image
 
 from app.common import hashers
 from app.photos.services.photo_analyzer_service_interface import (
@@ -30,3 +32,18 @@ class PhotoAnalyzerService(PhotoAnalyzerServiceInterface):
         encoder = CNN()
         encoding = encoder.encode_image(file_path)
         return encoding.dumps().hex()
+
+    def validate_image(self, file_path: str, extended_checks: bool = False) -> None:
+        if not filetype.is_image(file_path):
+            raise TypeError(f"{file_path} is not a valid image.")
+
+        if not extended_checks:
+            return
+
+        image = Image.open(file_path)
+        try:
+            image.transpose(Image.FLIP_LEFT_RIGHT)
+        except Exception:
+            raise TypeError(f"{file_path} is a corrupted image.")
+        finally:
+            image.close()
--- tests/unittests/apps/photos/services/test_photo_analyzer_service.py
+++ tests/unittests/apps/photos/services/test_photo_analyzer_service.py
@@ -1,6 +1,9 @@
+import filetype
 import numpy
 import pytest
 from imagededup.methods import CNN, AHash, DHash, PHash, WHash
+from mockito import mock
+from PIL import Image
 
 from app.common import hashers
 from app.photos.services.photo_analyzer_service import PhotoAnalyzerService
@@ -47,3 +50,32 @@ def test_analyzer_returns_cnn_encoding(pas: PhotoAnalyzerService, when):
     np_array = numpy.array([1, 2])
     when(CNN).encode_image(...).thenReturn(np_array)
     assert pas.encoding_cnn("testfile") == np_array.dumps().hex()
+
+
+def test_analyzer_should_return_succeed_when_valid_image_file_was_provided(
+    pas: PhotoAnalyzerService,
+    when,
+):
+    when(filetype).is_image(...).thenReturn(True)
+    pas.validate_image("/validimage.jpg", extended_checks=False)
+
+
+def test_analyzer_should_raise_when_verificatoin_detects_an_invalid_image(
+    pas: PhotoAnalyzerService,
+    when,
+):
+    when(filetype).is_image(...).thenReturn(False)
+    with pytest.raises(expected_exception=TypeError, match="valid"):
+        pas.validate_image("/invalidimage.jpg")
+
+
+def test_analyzer_should_raise_when_extended_verificatoin_detects_an_invalid_image(
+    pas: PhotoAnalyzerService,
+    when,
+):
+    when(filetype).is_image(...).thenReturn(True)
+    image = mock(Image.Image, strict=False)
+    when(Image).open(...).thenReturn(image)
+    when(image).transpose(...).thenRaise(Exception("test"))
+    with pytest.raises(expected_exception=Exception, match="corrupted"):
+        pas.validate_image("/invalidimage.jpg", extended_checks=True)
