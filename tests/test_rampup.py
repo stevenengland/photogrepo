08e4fce179408b5d1239cc83209d0d6f35f6c77f
StEn <github@steven-england.info>
2023-04-18 16:39:12 +0200

    fix: add AUTH_USER_MODEL

--- tests/test_rampup.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from django.contrib.auth.models import User
-
-
-@pytest.mark.django_db
-def test_user_creation():
-    User.objects.create_user("test", "test@example.com", "test")
-    assert User.objects.count() == 1
